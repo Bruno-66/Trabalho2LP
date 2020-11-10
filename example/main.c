fd32a664c4fad1da192898aabd76f8f2877e2454
Bruno dos Santos Alves <71350189+Bruno-66@users.noreply.github.com>
2020-11-10 10:18:22 -0300

    Add files via upload

---
+++ example/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "static_library.h"
+
+int main()
+{
+    int *v = malloc2(sizeof(int));
+    int *b = malloc2(sizeof(int));
+    *v = 10;
+    *b = 2;
+    printf("End de V: %d Valor: %d\n", v, *v);
+    printf("End de B: %d Valor: %d\n", b, *b);
+    atrib2(&v, &b);
+    printf("End de V: %d Valor: %d\n", v, *v);
+    printf("End de B: %d Valor: %d\n", b, *b);
+    dump();
+    printf("End de V: %d Valor: %d\n", v, *v);
+    printf("End de B: %d Valor: %d\n", b, *b);
+
+    return 0;
+}
